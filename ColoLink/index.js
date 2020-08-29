d5fc5571d0030bd705f917c53e7dbf03336db8b8
Shreyas Jaganmohan <drshrey@Shreyass-MacBook-Pro.local>
2020-08-28 23:35:46 -0700

    build works

--- ColoLink/index.js
+++ ColoLink/index.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import './index.css';
 
-/*
-    This file serves as a React button component
-    to easily include in your React app in order to start the access token
-    retrieval flow for the following supported providers:
-    - Google
-    - Slack
-*/
+const ColoLink = ({ coloApiKey, userId, disabled=false, integration, style, buttonText }) => {
+  const [isLoading, setIsLoading] = useState(false)  
+  let text = "Link Account"
+    if(integration === "twitter") {
+      text = "Link Twitter Account"
+    } else if(integration === 'quickbooks') {
+      text = 'Link QuickBooks Account'
+    } else if(integration === 'slack') {
+      text = 'Link Slack Account'
+    } else if(integration === 'google') {
+      text = 'Link Google Account'
+    }
+    
+    // buttonText overrides everything else
+    if(buttonText !== undefined) {
+      text = buttonText
+    }
+    
+    function sendCollateOauthRequest(apiKey, userId, integration){
+      setIsLoading(true)
+      console.log(apiKey, userId, integration);
+      // start oauth flow
+      // TODO: replace url with API_URL env var
+      axios.post('http://localhost:5000/oauth', {
+        integration: integration,
+        api_key: apiKey,
+        user_id: userId,
+      })
+      .then((data) => {
+        setIsLoading(false)
+        console.log(data)
+        console.log(data.data.authorize_url)
+        window.open(data.data.authorize_url, '_blank')
+      })
+      .catch((err) => {
+        alert(err)
+      })
+    }
 
-function ColoLink() {
     return (
-        <div>
-            ColoLink
-        </div>
+      <button 
+        className="CollateBtn" 
+        style={style} 
+        disabled={disabled || isLoading}
+        onClick={() => {sendCollateOauthRequest(coloApiKey, userId, integration)}}>
+        {isLoading ? 'Linking...' : text}
+      </button>
     )
 }
 
